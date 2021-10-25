6d0b41f74990afc0b635fccf22024e1b1c9bf9d6
赵文源 <antordragon@163.com>
2021-10-25 10:11:24 +0800

    1. 添加忽略文件

---
+++ sanyi-yoyo/yoyo-biz/src/main/java/com/sanyicloud/yoyo/mapper/DemoMapper.java
@@ -0,0 +1,8 @@
+package com.sanyicloud.yoyo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.sanyicloud.yoyo.entity.Demo;
+import org.apache.ibatis.annotations.Mapper;
+
+public interface DemoMapper extends BaseMapper<Demo> {
+}
